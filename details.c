ac3bc2258a2f68a78eabedd31f3c5b5cca02f061
sekar2002 <sowmiyasekar000@gmail.com>
2021-10-23 10:48:09 +0530

    first commit

---
+++ details.c
@@ -0,0 +1,38 @@
+#include stdio.h
+int main()
+{  char name[10];
+   int rollno;
+   char fathername[10];
+   char mothername[10];
+   int age;
+   long phoneno;
+   char mail[10];
+   char bloodgroup[5];
+   char address [10];
+   long adhaarno;
+   scanf("%s",name);
+   scanf("%d",&rollno);
+   scanf("%s",fathername);
+   scanf("%s",mothername);
+   scanf("%d",&age);
+   scanf("%l",&phoneno);
+   scanf("%s",mail);
+   scanf("%s",bloodgroup);
+   scanf("%s",address);
+   scanf("%l",phoneno);
+   printf("%s",name);
+   prinf("%d",&rollno);
+   printf("%s",fathername);
+   printf("%s",mothername);
+   printf("%d",&age);
+   printf"%l",&phoneno);
+   printf("%s",mail);
+   printf("%s",bloodgroup);
+   printf("%s",address);
+   printf"%l",phoneno);
+}
+
+		 
+
+
+

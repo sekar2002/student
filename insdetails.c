6902846a3116a98e5df714dd97516ec74d61ff6f
sekar2002 <sowmiyasekar000@gmail.com>
2021-10-23 11:11:01 +0530

    insurance commit

---
+++ insdetails.c
@@ -0,0 +1,26 @@
+  #include<stdio.h>
+  int main()
+{   char studentname[10];
+    char nomineename[10];
+    int nomineeage;
+    char relation[10];
+    long mobileno;
+    char nomineeoccupation[10];
+    long nominee aadharno;
+     scanf("%s",studentname);
+     scanf("%s",nomineename);
+     scanf("%s",nomineeage);
+     scanf("%s",relation);
+     scanf("%l",mobileno);
+     scanf("%s",nomineeoccupation);
+     scanf("%l",aadharno);
+     printf("%s",studentname);
+     printf("%s",nomineename);
+     printf("%d",nomineeage);
+     printf("%s",relation);
+     printf("%l",mobileno);
+     printf("%s",nomineeoccupation);
+     printf("%s",nomineeaadhaarno);
+}    
+
+

85cb65169e1b735d623a19a0fe1adbcd79ebb365
sekar2002 <sowmiyasekar000@gmail.com>
2021-10-23 11:31:03 +0530

    placement commit

---
+++ placedetails.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{  char name[10];
+   int rollno;
+   long mobileno;
+   char mail[10];
+   char resident;
+   int arrear;
+   float 1gpa;
+   float 2gpa;
+   float 3gpa;
+   scanf("%d",&1gpa);
+   scanf("%d",&2gpa);
+   scanf("%d",&3gpa);
+   scanf("%l",&mobileno);
+   scanf("%s",mail);
+   scanf("%d",&rollno);
+   scanf("%d",&arrear);
+
+
+
+
+

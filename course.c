c8a828b16bd2d65af539f0b67b9078ed0175e5f6
sekar2002 <sowmiyasekar000@gmail.com>
2021-10-25 18:52:53 +0530

    stash commit

---
+++ course.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() 
+{  int rollno;
+   int sem;
+   int onecredit;
+   int vad;
+   char vadname[10];
+   scanf("%d",&rollno);
+   scanf("%d",&sem);
+   scanf("%d",&onecredit);
+   scanf("%d",&vad);
+   scanf("%s",&vadname);
+   printf("%d",rollno);
+   printf("%d",sem);
+   printf("%d",onecredit);
+   printf("%d",vad);
+   printf("%d",vadname);
+}
+

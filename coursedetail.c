c01fdd446e0ef72e952a3eef1a0469a665f457c9
sekar2002 <sowmiyasekar000@gmail.com>
2021-10-26 18:52:00 +0530

    pull commit

---
+++ coursedetail.c
@@ -0,0 +1,12 @@
+int main()
+{ int rollno;
+  int sem;
+  int onecredit;
+  int vad;
+  char vadname[10];
+  scanf("%d",&rollno);
+ scanf("%d",&sem);
+ scanf("%d",&onecredit);
+ scanf("%d",&vad);
+ scanf("%c",&vadname);
+
